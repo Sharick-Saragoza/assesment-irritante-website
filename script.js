36e73c60b6664935690e8db91500a0d16351c92d
Sharick-Saragoza <150659105+Sharick-Saragoza@users.noreply.github.com>
2023-12-20 00:35:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Ihpone ID
+// Iphone ID
 const iphoneImage = document.getElementById('iphone');
 
-// Functie
+// (1) Functie laat de Iphone knipperen
 function knipperImage() {
     if (iphoneImage.style.opacity === '0') {
         iphoneImage.style.opacity = '1';
@@ -10,11 +10,21 @@ function knipperImage() {
     }
 }
 
-// Function to start blinking when the button is clicked
-function startKnipperen() {
+function startKnipper() {
 
     setInterval(knipperImage, 100);
 
 }
 
-document.getElementById('buttonIOS').addEventListener('click', startKnipperen);
+// Start de startKnipper functie wanneer je op de IOS button drukt
+document.getElementById('buttonIOS').addEventListener('click', startKnipper);
+
+// (2) Functie om automatisch te scrollen
+function autoScroll() {
+    setInterval(function() {
+        window.scrollBy(0, 2);
+    }, 20);
+}
+
+// Start de autoScroll functie wanneer je op de Android button drukt
+document.getElementById('buttonAndroid')?.addEventListener('click', autoScroll);
